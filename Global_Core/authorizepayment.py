c19306a4f8494965d57c2e5c4644724a32397993
ZainRaza116 <syedzaynraza@gmail.com>
2024-02-19 20:52:40 +0500

    NMI

--- Global_Core/authorizepayment.py
+++ Global_Core/authorizepayment.py
@@ -2,15 +2,16 @@
 Charge a credit card
 """
 
-
 import os
 import sys
-
 from authorizenet import apicontractsv1
 from authorizenet.apicontrollers import createTransactionController
 
-# CONSTANTS = imp.load_source('modulename', 'settings.py')
 
+# CONSTANTS = imp.load_source('modulename', 'settings.py')
+merchantAuth = apicontractsv1.merchantAuthenticationType()
+merchantAuth.name = '57njF7yrLVFs'
+merchantAuth.transactionKey = '42hTUkmRw78S987U'
 
 def charge_credit_card(amount, cardNumber , expirationDate , cardCod , firstName , lastName , company  , address , state , zip):
     """
@@ -19,9 +20,7 @@ def charge_credit_card(amount, cardNumber , expirationDate , cardCod , firstName
 
     # Create a merchantAuthenticationType object with authentication details
     # retrieved from the constants file
-    merchantAuth = apicontractsv1.merchantAuthenticationType()
-    merchantAuth.name = '57njF7yrLVFs'
-    merchantAuth.transactionKey = '42hTUkmRw78S987U'
+
 
     # Create the payment data for a credit card
     creditCard = apicontractsv1.creditCardType()
@@ -146,7 +145,148 @@ def charge_credit_card(amount, cardNumber , expirationDate , cardCod , firstName
     return response
 
 
+# if (os.path.basename(__file__) == os.path.basename(sys.argv[0])):
+#     # Corrected charge_credit_card function call
+#     charge_credit_card("100.0", "4111111111111111", "2035-12", "123", "zain", "raza", "digitolog", "model town", "txc",
+#                        "234")
+
+
+def authorize_credit_card(amount, cardNumber , expirationDate , cardCod , firstName , lastName , company  , address , state , zip):
+    """
+    Authorize a credit card (without actually charging it)
+    """
+
+    # Create a merchantAuthenticationType object with authentication details
+    # retrieved from the constants file
+    # merchantAuth = apicontractsv1.merchantAuthenticationType()
+    # merchantAuth.name = CONSTANTS.apiLoginId
+    # merchantAuth.transactionKey = CONSTANTS.transactionKey
+
+    # Create the payment data for a credit card
+    creditCard = apicontractsv1.creditCardType()
+    creditCard.cardNumber = cardNumber
+    creditCard.expirationDate = expirationDate
+    creditCard.cardCode = cardCod
+
+    # Add the payment data to a paymentType object
+    payment = apicontractsv1.paymentType()
+    payment.creditCard = creditCard
+
+    # Create order information
+    order = apicontractsv1.orderType()
+    order.invoiceNumber = "10101"
+    order.description = "Golf Shirts"
+
+    # Set the customer's Bill To address
+    customerAddress = apicontractsv1.customerAddressType()
+    customerAddress.firstName = firstName
+    customerAddress.lastName = lastName
+    customerAddress.company = company
+    customerAddress.address = address
+    customerAddress.city = "Pecan Springs"
+    customerAddress.state = state
+    customerAddress.zip = zip
+    customerAddress.country = "USA"
+
+    # Set the customer's identifying information
+    customerData = apicontractsv1.customerDataType()
+    customerData.type = "individual"
+    customerData.id = "99999456654"
+    customerData.email = "EllenJohnson@example.com"
+
+    # Add values for transaction settings
+    duplicateWindowSetting = apicontractsv1.settingType()
+    duplicateWindowSetting.settingName = "duplicateWindow"
+    duplicateWindowSetting.settingValue = "600"
+    settings = apicontractsv1.ArrayOfSetting()
+    settings.setting.append(duplicateWindowSetting)
+
+    # setup individual line items
+    line_item_1 = apicontractsv1.lineItemType()
+    line_item_1.itemId = "12345"
+    line_item_1.name = "first"
+    line_item_1.description = "Here's the first line item"
+    line_item_1.quantity = "2"
+    line_item_1.unitPrice = "12.95"
+    line_item_2 = apicontractsv1.lineItemType()
+    line_item_2.itemId = "67890"
+    line_item_2.name = "second"
+    line_item_2.description = "Here's the second line item"
+    line_item_2.quantity = "3"
+    line_item_2.unitPrice = "7.95"
+
+    # build the array of line items
+    line_items = apicontractsv1.ArrayOfLineItem()
+    line_items.lineItem.append(line_item_1)
+    line_items.lineItem.append(line_item_2)
+
+    # Create a transactionRequestType object and add the previous objects to it.
+    transactionrequest = apicontractsv1.transactionRequestType()
+    transactionrequest.transactionType = "authOnlyTransaction"
+    transactionrequest.amount = amount
+    transactionrequest.payment = payment
+    transactionrequest.order = order
+    transactionrequest.billTo = customerAddress
+    transactionrequest.customer = customerData
+    transactionrequest.transactionSettings = settings
+    transactionrequest.lineItems = line_items
+
+    # Assemble the complete transaction request
+    createtransactionrequest = apicontractsv1.createTransactionRequest()
+    createtransactionrequest.merchantAuthentication = merchantAuth
+    createtransactionrequest.refId = "MerchantID-0001"
+    createtransactionrequest.transactionRequest = transactionrequest
+    # Create the controller
+    createtransactioncontroller = createTransactionController(
+        createtransactionrequest)
+    createtransactioncontroller.execute()
+
+    response = createtransactioncontroller.getresponse()
+
+    if response is not None:
+        # Check to see if the API request was successfully received and acted upon
+        if response.messages.resultCode == "Ok":
+            # Since the API request was successful, look for a transaction response
+            # and parse it to display the results of authorizing the card
+            if hasattr(response.transactionResponse, 'messages') is True:
+                print(
+                    'Successfully created transaction with Transaction ID: %s'
+                    % response.transactionResponse.transId)
+                print('Transaction Response Code: %s' %
+                      response.transactionResponse.responseCode)
+                print('Message Code: %s' %
+                      response.transactionResponse.messages.message[0].code)
+                print('Description: %s' % response.transactionResponse.
+                      messages.message[0].description)
+            else:
+                print('Failed Transaction.')
+                if hasattr(response.transactionResponse, 'errors') is True:
+                    print('Error Code:  %s' % str(response.transactionResponse.
+                                                  errors.error[0].errorCode))
+                    print(
+                        'Error message: %s' %
+                        response.transactionResponse.errors.error[0].errorText)
+        # Or, print errors if the API request wasn't successful
+        else:
+            print('Failed Transaction.')
+            if hasattr(response, 'transactionResponse') is True and hasattr(
+                    response.transactionResponse, 'errors') is True:
+                print('Error Code: %s' % str(
+                    response.transactionResponse.errors.error[0].errorCode))
+                print('Error message: %s' %
+                      response.transactionResponse.errors.error[0].errorText)
+            else:
+                print('Error Code: %s' %
+                      response.messages.message[0]['code'].text)
+                print('Error message: %s' %
+                      response.messages.message[0]['text'].text)
+    else:
+        print('Null Response.')
+
+    return response
+
+
+
 if (os.path.basename(__file__) == os.path.basename(sys.argv[0])):
-    # Corrected charge_credit_card function call
-    charge_credit_card("100.0", "4111111111111111", "2035-12", "123", "zain", "raza", "digitolog", "model town", "txc",
-                       "234")
+    print("Hello World!")
+    authorize_credit_card("100.0", "4111111111111111", "2035-12", "123", "zain", "raza", "digitolog", "model town", "txc","234")
--- Global_Core/urls.py
+++ Global_Core/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('enter_payment_details/', views.charge_credit_card_view, name='enter_payment_details'),
+    path('nmi/', views.NMI, name='NMI'),
+    path('test/', views.test, name='test')
 ]
